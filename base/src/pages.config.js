28e8a7eae323668c2840954fb3f6d5038ce550ca
twolun <2237993760@qq.com>
2017-07-25 09:03:27 +0800

    Update

--- base/src/pages.config.js
+++ base/src/pages.config.js
@@ -13,7 +13,7 @@ export default [
 	},
 	{
 		title: '百度地图+echarts图表',
-		github: 'https://github.com/twolun/working/tree/master/projects/vue-area',
+		github: 'https://github.com/twolun/working/tree/master/projects/echart-map2',
 		demourl: '/echarts-map2.html',
 		viewimg: `${metadata.root}static/img/echarts-map2.png`
 	},
